c2947117259264244c30f2cb7a736fb16f7bd282
haroonmwanyare <63880961+haroonmwanyare@users.noreply.github.com>
2020-10-09 19:08:53 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+function dropdownMenu() {
+        var x = document.getElementById("dropdownClick");
+                if (x.className === "topnav") {
+                x.className += " responsive";
+                    /* changes topnav to topnav.responsive */
+                    
+                }
+                else {
+                    x.className = "topnav";
+                }
+            }
+
+
+
+
+ 
